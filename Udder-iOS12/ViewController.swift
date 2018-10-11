34ab56a63def93fbafa49322a4a0076dbe018e71
Dieter Bergmann <dieterbergmann@arcor.de>
2018-10-11 06:57:02 +0200

    Initial Commit

---
+++ Udder-iOS12/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder-iOS12
+//
+//  Created by Dieter Bergmann on 11.10.18.
+//  Copyright © 2018 Dieter Bergmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
